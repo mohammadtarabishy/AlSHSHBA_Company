7af20fc254c3d1b5ad6530fcdccbaebca851552b
Mohammed <mohammad9t6r@hotmail.com>
2022-03-20 14:28:13 +0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,202 @@
+<html lang="">
+<!-- To declare your language - read more here: https://www.w3.org/International/questions/qa-html-language-declarations -->
+<head>
+    <title>AL SHAHBA</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link href="{{asset('css/layout.css')}}" rel="stylesheet" type="text/css" media="all">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+</head>
+<body id="top">
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay " id="dev" style="background-image:url('{{asset('images/demo/backgrounds/2.jpg')}}');">
+    <header id="header" class="hoc clear">
+        <div id="logo" class="one_quarter first">
+            <h1><a href="index.html">شركة الشهباء</a></h1>
+            <p style="font-size: 14px">لتصنيع ماكينات الدرفلة على الحامي</p>
+        </div>
+        <div class="three_quarter">
+            <ul class="nospace clear">
+                <li class="one_third first">
+                    <div class="block clear"><a href="#"><i class="fas fa-phone"></i></a>
+                        <span><strong>Give us a call:</strong> +90 535 916 09 18</span></div>
+                </li>
+                <li class="one_third">
+                    <div class="block clear"><a class="faicon-facebook" href="#"><i class="fab fa-facebook"></i></a>
+                        <span><strong>Facebook:</strong><a
+                                href="https://www.facebook.com/ahmad.drhchini">ahmad.drhchini</a></span></div>
+                </li>
+                <li class="one_third">
+                    <div class="block clear"><a href="#"><i class="fas fa-clock"></i></a>
+                        <span><strong> Mon. - Sat.:</strong> 08.00am - 18.00pm</span></div>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <section id="navwrapper" class="hoc clear">
+        <nav id="mainav">
+            <ul class="clear">
+                <li class="active"><a href="index.html">الصفحة الرئيسية</a></li>
+{{--                        <li><a href="/gallery">معرض الاعمال</a></li>--}}
+{{--                </li>--}}
+{{--                <li><a class="drop" href="#">Dropdown</a>--}}
+{{--                    <ul>--}}
+{{--                        <li><a href="#">Level 2</a></li>--}}
+{{--                        <li><a class="drop" href="#">Level 2 + Drop</a>--}}
+{{--                            <ul>--}}
+{{--                                <li><a href="#">Level 3</a></li>--}}
+{{--                                <li><a href="#">Level 3</a></li>--}}
+{{--                                <li><a href="#">Level 3</a></li>--}}
+{{--                            </ul>--}}
+{{--                        </li>--}}
+{{--                        <li><a href="#">Level 2</a></li>--}}
+{{--                    </ul>--}}
+{{--                </li>--}}
+            </ul>
+            <form action="#"><select>
+                    <option selected="selected" value="">MENU</option>
+                    <option value="index.html">Home</option>
+                    <option value="#">Pages</option>
+                    <option value="pages/gallery.html">- - Gallery</option>
+                    <option value="pages/full-width.html">- - Full Width</option>
+                    <option value="pages/sidebar-left.html">- - Sidebar Left</option>
+                    <option value="pages/sidebar-right.html">- - Sidebar Right</option>
+                    <option value="pages/basic-grid.html">- - Basic Grid</option>
+                    <option value="pages/font-icons.html">- - Font Icons</option>
+                    <option value="#">Dropdown</option>
+                    <option value="#">- - Level 2</option>
+                    <option value="#">- - Level 2 + Drop</option>
+                    <option value="#">- - - Level 3</option>
+                    <option value="#">- - - Level 3</option>
+                    <option value="#">- - - Level 3</option>
+                    <option value="#">- - Level 2</option>
+                    <option value="#">Link Text</option>
+                    <option value="#">Link Text</option>
+                    <option value="#">Link Text</option>
+                    <option value="#">Long Link Text</option>
+                </select></form>
+        </nav>
+    </section>
+    <div id="pageintro" class="hoc clear">
+        <article>
+            <h3 class="heading">شركة الشهباء</h3>
+            <h3 class="heading">لتصنيع ماكينات الدرفلة على الحامي</h3>
+            <p>سلندرات ماكينات سحب_حديد سكب خامي تصنيع ماكينات
+            </p>
+            <footer><a class="btn" href="#">تركيا 55 Istanbul, Istanbul Province, Turkey
+                </a></footer>
+        </article>
+    </div>
+</div>
+<div class="wrapper row3">
+    <main class="hoc container clear">
+        <section id="introblocks">
+            <ul class="nospace group">
+                <li class="one_third">
+                    <figure><a class="imgover" href="#"><img style="width: 250px ;height: 250px"
+                                                             src="images/demo/a1.jfif" alt=""></a>
+                    </figure>
+                </li>
+                <li class="one_third">
+                    <figure><a class="imgover" href="#"><img style="width: 250px ;height: 250px"
+                                                             src="images/demo/a5.png" alt=""></a>
+                    </figure>
+                </li>
+                <li class="one_third">
+                    <figure><a class="imgover" href="#"><img style="width: 250px ;height: 250px"
+                                                             src="images/demo/a4.jfif" alt=""></a>
+                    </figure>
+                </li>
+                <li class="one_third">
+                    <figure><a class="imgover" href="#"><img style="width: 250px ;height: 250px"
+                                                             src="images/demo/a3.jfif" alt=""></a>
+                    </figure>
+                </li>
+                <li class="one_third">
+                    <figure><a class="imgover" href="#"><img style="width: 250px ;height: 250px"
+                                                             src="images/demo/a2.jfif" alt=""></a>
+                    </figure>
+                </li>
+                <li class="one_third">
+                    <figure><a class="imgover" href="#"><img style="width: 250px ;height: 250px"
+                                                             src="images/demo/a7.jfif" alt=""></a>
+                    </figure>
+                </li>
+                <li class="one_third">
+                    <figure><a class="imgover" href="#"><img style="width: 250px ;height: 250px"
+                                                             src="images/demo/a5.png" alt=""></a>
+                    </figure>
+                </li>
+                <li class="one_third">
+                    <figure><a class="imgover" href="#"><img style="width: 250px ;height: 250px"
+                                                             src="images/demo/a8.jfif" alt=""></a>
+                    </figure>
+                </li>
+                <li class="one_third">
+                    <figure><a class="imgover" href="#"><img style="width: 250px ;height: 250px"
+                                                             src="images/demo/a9.jfif" alt=""></a>
+                    </figure>
+                </li>
+            </ul>
+        </section>
+        <div class="clear"></div>
+    </main>
+</div>
+<div class="wrapper coloured">
+    <section id="testimonials" class="hoc container clear">
+        <article class="one_half first"><img style="width: 200px ;height: 200px" src="images/demo/ahmad.jpg" alt="">
+{{--                        <blockquote></blockquote>--}}
+            <h6 class="heading">Ahmad Derhshyni</h6>
+          </article>
+        <article class="one_half"><img style="width: 200px ;height: 200px" src="images/demo/abdo.jpg" alt="">
+{{--            <blockquote></blockquote>--}}
+            <h6 class="heading">Ahmad Derhshyni</h6>
+{{--            <em>Manager</em></article>--}}
+    </section>
+</div>
+{{--<div class="bgded overlay row4" style="background-image:url('images/demo/backgrounds/01.png');">--}}
+{{--    <footer id="footer" class="hoc clear">--}}
+{{--        <div class="center btmspace-50">--}}
+{{--            <h6 class="heading">شركة الشهباء</h6>--}}
+{{--            <ul class="faico clear">--}}
+{{--                <li><a class="faicon-dribble" href="#"><i class="fab fa-dribbble"></i></a></li>--}}
+{{--                <li><a class="faicon-facebook" href="#"><i class="fab fa-facebook"></i></a></li>--}}
+{{--                <li><a class="faicon-google-plus" href="#"><i class="fab fa-google-plus-g"></i></a></li>--}}
+{{--                <li><a class="faicon-linkedin" href="#"><i class="fab fa-linkedin"></i></a></li>--}}
+{{--                <li><a class="faicon-twitter" href="#"><i class="fab fa-twitter"></i></a></li>--}}
+{{--                <li><a class="faicon-vk" href="#"><i class="fab fa-vk"></i></a></li>--}}
+{{--            </ul>--}}
+{{--        </div>--}}
+{{--    </footer>--}}
+{{--</div>--}}
+<div class="wrapper row5">
+    <div id="copyright" class="hoc clear">
+        <p class="fl_left"></p>
+    </div>
+</div>
+<a id="backtotop" href="#top" class=""><i class="fas fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script>
+    $(document).ready(function () {
+        var images = [
+            "{{asset('images/demo/backgrounds/2.jpg')}}",
+            "{{asset('images/demo/backgrounds/3.jpg')}}",
+            "{{asset('images/demo/backgrounds/4.jpg')}}"
+        ]
+        var imageHead = document.getElementById("dev");
+        var i = 0;
+        setInterval(function () {
+            imageHead.style.backgroundImage = "url(" + images[i] + ")";
+            i = i + 1;
+            if (i === images.length) {
+                i = 0;
+            }
+        }, 10000);
+    });
+</script>
+<script src="{{asset('scripts/jquery.min.js')}}"></script>
+<script src="{{asset('scripts/jquery.backtotop.js')}}"></script>
+<script src="{{asset('scripts/jquery.mobilemenu.js')}}"></script>
+</body>
+</html>
